98e420344ed05be1155d379658cf7bab944d47a5
Emma3434 <51844300+Emma3434@users.noreply.github.com>
2020-05-01 11:02:54 -0600

    no restriction

--- server.js
+++ server.js
@@ -285,7 +285,7 @@ router.route('/movies')
 
     .post(authJwtController.isAuthenticated, function(req,res)
     {
-        if (!req.body.title||!req.body.year_released||!req.body.genre||!req.body.actors) {
+        if (!req.body.title||!req.body.year_released||!req.body.genre) {
             res.json({success: false, message: 'Please enter ALL the necessary fields: title, year released, genre, actor name 1,' +
                     'character name 1, actor name 2, character name 2, actor name 3, character name 3.'});
         }
